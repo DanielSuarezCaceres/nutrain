a7ba640f9d65852792810117d28a0d920bd43afd
DanielSuarezCaceres <daniel.suarezcac@gmail.com>
2019-12-27 19:44:18 +0000

    User model and controller created. Migration done

---
+++ db/migrate/20191227194021_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.text :name
+      t.text :lastname
+      t.text :phone
+      t.text :city
+      t.text :country
+      t.text :type
+
+      t.timestamps
+    end
+  end
+end
