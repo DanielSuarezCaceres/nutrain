8f1ab23aba71c007440375bc628c484fac7a38d7
Daniel Suarez Caceres <daniel.suarezcac@gmail.com>
2020-04-29 00:31:07 +0200

    Refactor code in menus and professional actions. Add meals, foods, workouts & exercises history view

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -32,13 +32,7 @@ class MealsController < ApplicationController
     user = current_user
     @meal = user.meals.new(meal_params)
     # check existing ingredients sent by select2
-    byebug
-    if params[:meal][:food_ids]
-      params[:meal][:food_ids].each do |ingredient|
-        @meal.foods << Food.find(ingredient) unless @meal.foods.include? Food.find(ingredient)
-      end
-    end
-    byebug
+    add_existing_ingredients
     if @meal.valid?
       @meal.save
     end
@@ -48,13 +42,17 @@ class MealsController < ApplicationController
   # PATCH/PUT /meals/1
   # PATCH/PUT /meals/1.json
   def update
+    #if params[:meal][:food_ids]
+    #  params[:meal][:food_ids].each do |ingredient|
+    #    @meal.foods << Food.find(ingredient) unless @meal.foods.include? Food.find(ingredient)
+    #  end
+    #end
+    update_ingredients
     byebug
-    if params[:meal][:food_ids]
-      params[:meal][:food_ids].each do |ingredient|
-        @meal.foods << Food.find(ingredient) unless @meal.foods.include? Food.find(ingredient)
-      end
-    end
     if @meal.update(meal_params)
+      #if params[:meal][:food_ids]
+      #  update_meal_foods
+      #end
       redirect_to root_path, notice: 'Meal updated successfully'
     else
       render :edit
@@ -96,8 +94,46 @@ class MealsController < ApplicationController
         :nut_free,
         :soy_free,
         :user_id,
-        foods_attributes: %i[id name brand kcal protein carbs fats serving_size vegan vegetarian gluten_free nut_free soy_free _destroy]
+        foods_attributes: %i[id name brand kcal protein carbs fats serving_size vegan vegetarian gluten_free nut_free soy_free _destroy],
+        food_ids: []
     )
+  end
+
+  def update_meal_foods
+    new_ingredients = []
+    params[:meal][:food_ids].each do |ing_id|
+      new_ingredients << Food.find(ing_id)
+    end
+    
+    @meal.foods.each do |food|
+      @meal.foods.delete(food) unless new_ingredients.include? food
+      #if !new_ingredientes.include? food then @meal.foods.delete(food)
+    end
+    return
+  end
+
+  def update_ingredients
+    selected_ingredients_ids = params[:meal][:food_ids]
+    params[:meal][:foods_attributes].each do |key, value|
+      byebug
+      if value["id"].nil?
+        next
+      else
+        byebug
+        params[:meal][:foods_attributes].delete(key) unless selected_ingredients_ids.include? value["id"]
+      end
+    end
+    byebug
+    aux = true
+  end
+
+  def add_existing_ingredients
+    if params[:meal][:food_ids]
+      params[:meal][:food_ids].each do |ingredient|
+        @meal.foods << Food.find(ingredient) unless @meal.foods.include? Food.find(ingredient)
+      end
+    end
+    return
   end
 
 end
