063d2c76895d7ab8d5e64d2e15a25f96701f41b1
Daniel Suarez Caceres <daniel.suarezcac@gmail.com>
2020-06-10 22:28:21 +0100

    Fix some names in measurement graphics

--- app/javascript/custom/measurements.js
+++ app/javascript/custom/measurements.js
@@ -7,17 +7,6 @@
 });*/
 
 document.addEventListener("turbolinks:load", function () {
-  $("#button-measurements-year").click(function () {
-    console.log("SHOWING YEARLY");
-    // $("#measurements-graphics-year").hide();
-    // $("#measurements-graphics-month").show();
-    document.getElementById('measurements-graphics-year').style.display = 'inline';
-    document.getElementById('measurements-graphics-month').style.display = 'none';
-    document.getElementById('measurements-graphics-quarter').style.display = 'none';
-  })
-})
-
-document.addEventListener("turbolinks:load", function () {
   $("#button-measurements-month").click(function () {
     console.log("SHOWING MONTH");
     // $("#measurements-graphics-month").show();
@@ -28,18 +17,29 @@ document.addEventListener("turbolinks:load", function () {
     var xRotation = chart["chart"]["scales"]["x-axis-0"];
     var aspect = chart["chart"]["chart"]["aspectRatio"]
     document.getElementById('measurements-graphics-month').style.display = 'inline';
-    document.getElementById('measurements-graphics-year').style.display = 'none';
-    document.getElementById('measurements-graphics-quarter').style.display = 'none';
+    document.getElementById('measurements-graphics-six-months').style.display = 'none';
+    document.getElementById('measurements-graphics-three-months').style.display = 'none';
   })
 })
 
 document.addEventListener("turbolinks:load", function () {
-  $("#button-measurements-quarter").click(function () {
+  $("#button-measurements-three-months").click(function () {
     console.log("SHOWING QUARTER");
     // $("#measurements-graphics-month").show();
     // $("#measurements-graphics-year").hide();
-    document.getElementById('measurements-graphics-quarter').style.display = 'inline';
-    document.getElementById('measurements-graphics-year').style.display = 'none';
+    document.getElementById('measurements-graphics-three-months').style.display = 'inline';
+    document.getElementById('measurements-graphics-six-months').style.display = 'none';
     document.getElementById('measurements-graphics-month').style.display = 'none';
+  })
+})
+
+document.addEventListener("turbolinks:load", function () {
+  $("#button-measurements-six-months").click(function () {
+    console.log("SHOWING YEARLY");
+    // $("#measurements-graphics-year").hide();
+    // $("#measurements-graphics-month").show();
+    document.getElementById('measurements-graphics-six-months').style.display = 'inline';
+    document.getElementById('measurements-graphics-month').style.display = 'none';
+    document.getElementById('measurements-graphics-three-months').style.display = 'none';
   })
 })
